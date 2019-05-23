fe793c03e37dc3e406fe18c07cfe466c267a8a7a
anggi03 <mynameanggi03@gmail.com>
2019-05-23 23:58:28 +0700

    UTS AKB

---
+++ app/src/main/java/com/e/uts_akb_if7_10116288/MainActivity.java
@@ -0,0 +1,57 @@
+package com.e.uts_akb_if7_10116288;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                        case R.id.nav_contact:
+                            selectedFragment = new ContactFragment();
+                            break;
+                        case R.id.nav_friendlist:
+                            selectedFragment = new FriendlistFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+        public void OpenInstagram(View view) {
+            String url = "https://instagram.com/rezamuzaky22";
+            Intent bukainstagram = new Intent(Intent.ACTION_VIEW);
+            bukainstagram.setData(Uri.parse(url));
+            startActivity(bukainstagram);
+        }
+}
